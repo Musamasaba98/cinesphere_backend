9dfdd99582fbe563c02f455db01ff204ea879fe6
Masaba Musa <masamata98@gmail.com>
2023-03-25 04:15:45 +0300

    Add router middleware to the App.use

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import cors from 'cors'
 import userRouter from "./src/routes/user.route.js"
 import movieRouter from "./src/routes/movie.route.js"
 import genreRouter from "./src/routes/genre.route.js"
+import personRouter from "./src/routes/person.route.js"
+import movieCastRouter from "./src/routes/movieCast.route.js"
+import languageRouter from "./src/routes/language.route.js"
+import keywordRouter from "./src/routes/keyword.route.js"
+import jobTitleRouter from "./src/routes/jobTitle.route.js"
+import departmentRouter from "./src/routes/department.route.js"
+import companyRouter from "./src/routes/company.route.js"
 import errorHandler from "./src/middlewares/errorHandler.js";
 import customError from "./src/utils/customError.js";
 
@@ -16,6 +23,13 @@ app.use(express.urlencoded({ limit: '1gb', extended: true }));
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/movies", movieRouter)
 app.use("/api/v1/genre", genreRouter)
+app.use("/api/v1/person", personRouter)
+app.use("/api/v1/movieCast", movieCastRouter)
+app.use("/api/v1/laguage", languageRouter)
+app.use("/api/v1/keyword", keywordRouter)
+app.use("/api/v1/jobTitle", jobTitleRouter)
+app.use("/api/v1/department", departmentRouter)
+app.use("/api/v1/company", companyRouter)
 app.use(errorHandler)
 app.get('/', (req, res) => {
     res.send('Hello welcome to CineSphere Backend API')
